2a9c309e513ba884616388f7fbcfc60fd503f2f5
Yujun Liu <dreamzack@gmail.com>
2019-07-24 22:43:46 -0700

    added router

---
+++ packages/web/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import * as React from "react";
+import { RegisterConnector } from "../modules/register/RegisterConnector";
+
+export const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact={true} path="/register" component={RegisterConnector} />
+    </Switch>
+  </BrowserRouter>
+);
