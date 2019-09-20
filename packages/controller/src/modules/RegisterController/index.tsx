0caa0de1cba4f55f54191febbf62b97be5a4150f
Yujun Liu <dreamzack@gmail.com>
2019-09-19 23:47:38 -0700

    finished part 7 connector

---
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Props {
+  children: (data: {
+    submit: (values: any) => Promise<null>;
+  }) => JSX.Element | null;
+}
+export class RegisterController extends React.PureComponent<Props> {
+  submit = async (values: any) => {
+    console.log('yl-RegisterController', values);
+    return null;
+  };
+  render() {
+    return this.props.children({ submit: this.submit });
+  }
+}
--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { RegisterView } from './ui/RegisterView';
+import { RegisterController } from '@abb/controller';
 
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return (
+      <>
+        <RegisterController>
+          {({ submit }) => <RegisterView submit={submit} />}
+        </RegisterController>
+      </>
+    );
   }
 }
